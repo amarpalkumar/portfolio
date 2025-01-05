5f6cd92a779c4fca8cb699d7d9702d48c91b5a99
amarpalkumar <amarpalkumar5@gmail.com>
2025-01-05 13:50:45 +0530

    Update script.js
    show contact in mobile view and on click toggle work

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,10 @@ const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // Sidebar toggle functionality for mobile
 if (sidebar && sidebarBtn) {
-  // sidebarBtn.addEventListener("click", function () { 
-    elementToggleFunc(sidebar); 
-  // });
+  elementToggleFunc(sidebar);
+  sidebarBtn.addEventListener("click", function () { 
+  elementToggleFunc(sidebar); 
+  });
 }
 
 // Testimonials variables
